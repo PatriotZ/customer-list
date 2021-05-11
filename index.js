4f18bda8dbf64fc6a1b210327e4a2cae8219b660
Zafar Akhtari <zafarakhtari@live.com>
2021-05-11 08:03:19 +0600

    added .env

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+require('dotenv').config()
+
 // Add required packages
 const express = require("express");
 const app = express();
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 // Set up EJS
 app.set("view engine", "ejs");
@@ -12,7 +21,6 @@ app.listen(process.env.PORT || 3000, () => {
 });
 
 // Setup routes
-app.get("/", (req, res) => {
-    res.send ("Hello world...");
-   // res.render("index");
+app.get("/", (req, res) => {    
+   res.render("index");
 });
